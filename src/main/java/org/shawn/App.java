fbfec1c748c3d719f4bc8eedd3cd199e4d68b03b
peixy <shawnpei0313@gmail.com>
2023-11-09 18:49:45 -0500

    Build the backend backbone

---
+++ src/main/java/org/shawn/App.java
@@ -0,0 +1,13 @@
+package org.shawn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
